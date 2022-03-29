38823467a5e599e9666d12bf80e17d1e471dbe7c
Matt Baker <matthew.baker4175@gmail.com>
2022-03-28 23:48:06 -0400

    fix merge conflict

--- src/app/game-window/game-window.component.spec.ts
+++ src/app/game-window/game-window.component.spec.ts
@@ -1,11 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameWindowComponent } from './game-window.component';
 
-<<<<<<< HEAD
-import { GameWindowComponent } from './game-window.component';
-
-=======
->>>>>>> main
 describe('GameWindowComponent', () => {
   let component: GameWindowComponent;
   let fixture: ComponentFixture<GameWindowComponent>;
