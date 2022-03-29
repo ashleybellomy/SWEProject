2f0ae2f5eb0fb40b82862fee53109c38eb5fe3e2
Matt Baker <matthew.baker4175@gmail.com>
2022-03-28 23:27:53 -0400

    fix test

--- src/app/game-window/game-window.component.spec.ts
+++ src/app/game-window/game-window.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { GameWindowComponent } from './game-window.component';
 
 import { GameComponent } from './game.component';
 
 describe('GameComponent', () => {
   let component: GameComponent;
-  let fixture: ComponentFixture<GameComponent>;
+  let fixture: ComponentFixture<GameWindowComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
