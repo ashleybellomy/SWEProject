93f4b64fe07405768cee05679a6ec0b5de9ad106
Matt Baker <matthew.baker4175@gmail.com>
2022-04-03 12:15:13 -0400

    get player to move based on the date, also refresh board when month changes.

--- src/app/core/models/player.ts
+++ src/app/core/models/player.ts
@@ -1,3 +1,7 @@
+import { Game, Tweens } from "phaser";
+
+// TODO: consider if some of this should just be in Board class
+// it feels like player class should just be player data, not physics
 export class Player {
 
     constructor(jobTitle: string, startingSalary: number, startingCash: number) {
@@ -6,9 +10,42 @@ export class Player {
         this.cash = startingCash;
     }
 
-    //TODO: do we want to capture a name?
-    //public name: string;
     public jobTitle: string;
     public monthlySalary: number;
     public cash: number;
+    private location: Phaser.Math.Vector2;
+    //TODO: this doesn't really belong here, move or delete
+    private scene: Phaser.Scene;
+    private phys: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    
+    //TODO: some of this should just happen in the constructor
+    setLocation(scene: Phaser.Scene, x: number, y: number, squareSize: number) {
+        this.scene = scene;
+
+        this.phys = this.scene.physics.add.sprite(x, y, "player");
+        this.phys.setDisplaySize(squareSize, squareSize);
+        
+        this.location = new Phaser.Math.Vector2(x, y);
+    }
+
+    public updateLocation(x: number, y: number) {
+        console.log('update ' + x + ' ' + y);
+        this.location.x = x;
+        this.location.y = y;
+        this.scene.physics.moveToObject(this.phys, this.location, 500);
+    }
+
+    update() {
+        var distance = Phaser.Math.Distance.Between(this.phys.x, this.phys.y, this.location.x, this.location.y);
+
+        if (this.phys.body.speed > 0)
+        {
+            //  4 is our distance tolerance, i.e. how close the source can get to the target
+            //  before it is considered as being there. The faster it moves, the more tolerance is required.
+            if (distance < 4)
+            {
+                this.phys.body.reset(this.location.x, this.location.y);
+            }
+        }
+    }
 }
