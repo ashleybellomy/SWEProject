99dbc82e09acc22a2ac05ba3f7e14dd6179beabb
JofaTech <80646647+JofaTech@users.noreply.github.com>
2022-04-19 20:32:41 -0400

    Added a few jobs and randomized them. Push test.

--- src/app/core/services/job-generator.service.ts
+++ src/app/core/services/job-generator.service.ts
@@ -8,6 +8,29 @@ export class JobGeneratorService {
     generateRandomJob(): Job {
         // TODO: the idea here is eventually we will have some list of jobs of different tiers
         // and we will randomly choose one to make it interesting each time.
-        return new Job('Barista', 'Starbucks', 1200);
+        var jobNumber = Phaser.Math.Between(1, 5);
+
+        if (jobNumber == 1)
+        {
+            return new Job('Barista', 'Starbucks', 1200);
+        }
+        else if (jobNumber == 2)
+        {
+          return new Job('Shelf Stocker', 'Publix', 1000);
+        }
+        else if (jobNumber == 3)
+        {
+          return new Job('Pizza Delivery Person', 'Pizza Hut', 800);
+        }
+        else if (jobNumber == 4)
+        {
+          return new Job('Sales Professional', 'Suck It Vacuums', 1600);
+        }
+        else if (jobNumber == 5)
+        {
+          return new Job('Assistant Manager', 'Walgreens', 2000);
+        }
+
+        return new Job('Vagrant', 'The Streets', 500);
     }
 }
